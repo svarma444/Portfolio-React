fa4271c398e92c686f7470e3033af696f44f74f9
swapnith.varma <your.personal@email.com>
2026-01-15 19:25:46 +0530

    Changes in Highlights section in both jsx and styling.

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,14 +1,49 @@
-export default function Highlights() {
-  return (
-    <section className="card">
-      <h2>Highlights</h2>
+export default function Highlights(){
+  return( 
+     <section className="card">
+        <h2>About</h2>
 
-      <div className="row">
-        <div className="pill">5+ years experience</div>
-        <div className="pill">C++, C#, JavaScript</div>
-        <div className="pill">PLM & CAD</div>
-        <div className="pill">Cloud & .NET</div>
-      </div>
-    </section>
+        <p className="bio">
+        Software Engineer specializing in C++, C#, SQL, CAD Integrations and PLM workflow engineering.
+        Experienced in system optimization, multi-threaded components, and resolving complex defects through deep
+        debugging and data-level analysis, with a strong ability to quickly grasp new requirements and adapt to
+        evolving project needs.
+        </p>
+
+        <div className="stats">
+          <div className="stat">
+            <strong>5</strong>
+            <span>Years Experience</span>
+          </div>
+
+          <div className="stat">
+            <strong>10+</strong>
+            <span>Projects Delivered</span>
+          </div>
+
+          <div className="stat">
+            <strong>3</strong>
+            <span>Tech Stacks</span>
+          </div>
+        </div>
+
+     </section>
   );
 }
+
+
+
+// export default function Highlights() {
+//   return (
+//     <section className="card">
+//       <h2>Highlights</h2>
+
+//       <div className="row">
+//         <div className="pill">5+ years experience</div>
+//         <div className="pill">C++, C#, JavaScript</div>
+//         <div className="pill">PLM & CAD</div>
+//         <div className="pill">Cloud & .NET</div>
+//       </div>
+//     </section>
+//   );
+// }
